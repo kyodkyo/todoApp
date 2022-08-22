8272f970099e87fcd3906fa72e00f310068cdf6f
kyodkyo <dkekry@naver.com>
2022-08-22 23:05:28 +0900

    test

---
+++ src/main/java/com/codestates/todoApp/TodoAppApplication.java
@@ -0,0 +1,20 @@
+package com.codestates.todoApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class TodoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoAppApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String init(){
+		return "To-do Application!";
+	}
+}
