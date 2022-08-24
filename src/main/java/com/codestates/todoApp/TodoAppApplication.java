07814c0ae81b556ddbc4849e499388035c8200fa
kyodkyo <dkekry@naver.com>
2022-08-24 16:11:37 +0900

    update

--- src/main/java/com/codestates/todoApp/TodoAppApplication.java
+++ src/main/java/com/codestates/todoApp/TodoAppApplication.java
@@ -2,8 +2,17 @@ package com.codestates.todoApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.*;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 @SpringBootApplication
 @RestController
@@ -13,8 +22,21 @@ public class TodoAppApplication {
 		SpringApplication.run(TodoAppApplication.class, args);
 	}
 
-	@GetMapping("/")
+	@GetMapping("/hello")
 	public String init(){
-		return "To-do Application!";
+
+		return "hello";
 	}
+
+//	@GetMapping
+//	public ResponseEntity<Map<String, String>> hello(){
+//		Map<String, String> data = new HashMap<>();
+//		data.put("text", "hello team 30");
+//		data.put("id", "52");
+//
+//		Map<String, Object> map = new HashMap<>();
+//		map.put("hello", data);
+//
+//		return new HashMap<>(map, )
+//	}
 }
